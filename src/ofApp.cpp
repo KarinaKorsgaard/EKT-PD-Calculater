4c67222b94b23158ceb5b61a3814005ccc4d8911
KarinaKorsgaard <kkorsgaardjensen@gmail.com>
2016-04-13 16:28:45 +0300

    framerate cap

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -14,11 +14,11 @@ void ofApp::setup(){
     clipboardContent = ofxClipboard::paste(); //<<<<<<<<<<<<<<<<<
     sendTo = ofToInt(clipboardContent);
     
-//    newClipboardContent = ofSystemTextBoxDialog("IP");
-//    
-//    ofxClipboard::copy(newClipboardContent); //<<<<<<<<<<<<<<<<<
-//    clipboardContent = ofxClipboard::paste(); //<<<<<<<<<<<<<<<<<
-//    host = ofToInt(clipboardContent);
+    //    newClipboardContent = ofSystemTextBoxDialog("IP");
+    //
+    //    ofxClipboard::copy(newClipboardContent); //<<<<<<<<<<<<<<<<<
+    //    clipboardContent = ofxClipboard::paste(); //<<<<<<<<<<<<<<<<<
+    //    host = ofToInt(clipboardContent);
     
     nameStuff();
     receiver.setup(port);
@@ -33,15 +33,17 @@ void ofApp::setup(){
             buttons.push_back(b);
         }
     }
+    ofSetFrameRate(30);
     
 }
 
 //--------------------------------------------------------------
 void ofApp::update(){
+    bool send = false;
     
     ofxOscBundle bundle;
     while(receiver.hasWaitingMessages()){
-        
+        send = true;
         ofxOscMessage sendMessage;
         ofxOscMessage msg;
         
@@ -70,7 +72,7 @@ void ofApp::update(){
                         sendMessage.clear();
                         
                         
-
+                        
                     }
                 }
                 
@@ -94,89 +96,90 @@ void ofApp::update(){
                     bundle.addMessage(sendMessage);
                     sendMessage.clear();
                     
-               
+                    
                     
                     
                 }
             }
         }
-       
+        
     }
     
-    
-    sender.sendBundle(bundle);
-    bundle.clear();
-    
-
-    //averaged values;
-    float total = 0;
-    float xAdded = 0;
-    float yAdded = 0;
-    float rotAdded = 0;
-    
-    vector<float>sepAdddedX;
-    vector<float>sepAdddedY;
-    vector<float>sepAdddedRot;
-    
-    vector<float>totalSep;
-    
-    sepAdddedX.resize(6);
-    sepAdddedY.resize(6);
-    sepAdddedRot.resize(6);
-    
-    totalSep.resize(6);
-
-    
-    for(int i = 0; i<buttons.size();i++){
-        if(buttons[i].on){
-            total ++;
-            xAdded += buttons[i].x;
-            yAdded += buttons[i].y;
-            rotAdded+= buttons[i].rot;
-            for(int u = 0; u<6;u++){
-                if(buttons[i].obj==u){
-                    totalSep[u]++;
-                    sepAdddedX[u]+=buttons[i].x;
-                    sepAdddedY[u]+=buttons[i].y;
-                    sepAdddedRot[u]+=buttons[i].rot;
+    if(send){
+        sender.sendBundle(bundle);
+        bundle.clear();
+        
+        
+        //averaged values;
+        float total = 0;
+        float xAdded = 0;
+        float yAdded = 0;
+        float rotAdded = 0;
+        
+        vector<float>sepAdddedX;
+        vector<float>sepAdddedY;
+        vector<float>sepAdddedRot;
+        
+        vector<float>totalSep;
+        
+        sepAdddedX.resize(6);
+        sepAdddedY.resize(6);
+        sepAdddedRot.resize(6);
+        
+        totalSep.resize(6);
+        
+        
+        for(int i = 0; i<buttons.size();i++){
+            if(buttons[i].on){
+                total ++;
+                xAdded += buttons[i].x;
+                yAdded += buttons[i].y;
+                rotAdded+= buttons[i].rot;
+                for(int u = 0; u<6;u++){
+                    if(buttons[i].obj==u){
+                        totalSep[u]++;
+                        sepAdddedX[u]+=buttons[i].x;
+                        sepAdddedY[u]+=buttons[i].y;
+                        sepAdddedRot[u]+=buttons[i].rot;
+                    }
                 }
             }
         }
+        ofxOscMessage m;
+        m.setAddress("/total");
+        m.addFloatArg(total);
+        for(int i = 0; i<totalSep.size();i++){
+            m.addFloatArg(totalSep[i]);
+        }
+        bundle.addMessage(m);
+        m.clear();
+        
+        m.setAddress("/X");
+        m.addFloatArg(xAdded);
+        for(int i = 0; i<sepAdddedX.size();i++){
+            m.addFloatArg(sepAdddedX[i]);
+        }
+        bundle.addMessage(m);
+        m.clear();
+        
+        m.setAddress("/Y");
+        m.addFloatArg(yAdded);
+        for(int i = 0; i<sepAdddedX.size();i++){
+            m.addFloatArg(sepAdddedY[i]);
+        }
+        bundle.addMessage(m);
+        m.clear();
+        
+        m.setAddress("/Rot");
+        m.addFloatArg(rotAdded);
+        for(int i = 0; i<sepAdddedX.size();i++){
+            m.addFloatArg(sepAdddedRot[i]);
+        }
+        bundle.addMessage(m);
+        m.clear();
+        senddedoscMessage = ofToString(total);
+        sender.sendBundle(bundle);
     }
-    ofxOscMessage m;
-    m.setAddress("/total");
-    m.addFloatArg(total);
-    for(int i = 0; i<totalSep.size();i++){
-        m.addFloatArg(totalSep[i]);
-    }
-    bundle.addMessage(m);
-    m.clear();
-    
-    m.setAddress("/X");
-    m.addFloatArg(xAdded);
-    for(int i = 0; i<sepAdddedX.size();i++){
-        m.addFloatArg(sepAdddedX[i]);
-    }
-    bundle.addMessage(m);
-    m.clear();
-    
-    m.setAddress("/Y");
-    m.addFloatArg(yAdded);
-    for(int i = 0; i<sepAdddedX.size();i++){
-        m.addFloatArg(sepAdddedY[i]);
-    }
-    bundle.addMessage(m);
-    m.clear();
-    
-    m.setAddress("/Rot");
-    m.addFloatArg(rotAdded);
-    for(int i = 0; i<sepAdddedX.size();i++){
-        m.addFloatArg(sepAdddedRot[i]);
-    }
-    bundle.addMessage(m);
-    m.clear();
-    senddedoscMessage = ofToString(total);
-    sender.sendBundle(bundle);
     
     ofSetWindowTitle("Framerate: "+ofToString(ofGetFrameRate(),0));
     
@@ -191,92 +194,92 @@ void ofApp::draw(){
     ofDrawBitmapString("from port " +ofToString(port), 10, 40);
     ofDrawBitmapString("on ip localhost", 10, 60);
     ofDrawBitmapString(oscMessage, 10, 80);
-
     
-
+    
+    
 }
 
 //--------------------------------------------------------------
 void ofApp::keyPressed(int key){
-//    ofxOscBundle bundle;
-//    for(int i = 0 ; i<buttons.size();i++){
-//        buttons[i].on = true;
-//        buttons[i].x = ofRandom(1);
-//        buttons[i].y = ofRandom(1);
-//        buttons[i].rot = ofRandom(1);
-//        
-//        
-//        
-//        ofxOscMessage sendMessage;
-//        
-//        sendMessage.setAddress("/toVDMX/"+ofToString(buttons[i].table)+"/"+ofToString(buttons[i].obj)+"/ROT");
-//        sendMessage.addFloatArg(buttons[i].rot);
-//        bundle.addMessage(sendMessage);
-//        sendMessage.clear();
-//        
-//        sendMessage.setAddress("/toVDMX/"+ofToString(buttons[i].table)+"/"+ofToString(buttons[i].obj)+"/X");
-//        sendMessage.addFloatArg(buttons[i].x);
-//        bundle.addMessage(sendMessage);
-//        sendMessage.clear();
-//        
-//        sendMessage.setAddress("/toVDMX/"+ofToString(buttons[i].table)+"/"+ofToString(buttons[i].obj)+"/Y");
-//        sendMessage.addFloatArg(buttons[i].y);
-//        bundle.addMessage(sendMessage);
-//        sendMessage.clear();
-//    }
-//    sender.sendBundle(bundle);
+    //    ofxOscBundle bundle;
+    //    for(int i = 0 ; i<buttons.size();i++){
+    //        buttons[i].on = true;
+    //        buttons[i].x = ofRandom(1);
+    //        buttons[i].y = ofRandom(1);
+    //        buttons[i].rot = ofRandom(1);
+    //
+    //
+    //
+    //        ofxOscMessage sendMessage;
+    //
+    //        sendMessage.setAddress("/toVDMX/"+ofToString(buttons[i].table)+"/"+ofToString(buttons[i].obj)+"/ROT");
+    //        sendMessage.addFloatArg(buttons[i].rot);
+    //        bundle.addMessage(sendMessage);
+    //        sendMessage.clear();
+    //
+    //        sendMessage.setAddress("/toVDMX/"+ofToString(buttons[i].table)+"/"+ofToString(buttons[i].obj)+"/X");
+    //        sendMessage.addFloatArg(buttons[i].x);
+    //        bundle.addMessage(sendMessage);
+    //        sendMessage.clear();
+    //
+    //        sendMessage.setAddress("/toVDMX/"+ofToString(buttons[i].table)+"/"+ofToString(buttons[i].obj)+"/Y");
+    //        sendMessage.addFloatArg(buttons[i].y);
+    //        bundle.addMessage(sendMessage);
+    //        sendMessage.clear();
+    //    }
+    //    sender.sendBundle(bundle);
     
     
 }
 
 //--------------------------------------------------------------
 void ofApp::keyReleased(int key){
-
+    
 }
 
 //--------------------------------------------------------------
 void ofApp::mouseMoved(int x, int y ){
-
+    
 }
 
 //--------------------------------------------------------------
 void ofApp::mouseDragged(int x, int y, int button){
-
+    
 }
 
 //--------------------------------------------------------------
 void ofApp::mousePressed(int x, int y, int button){
-
+    
 }
 
 //--------------------------------------------------------------
 void ofApp::mouseReleased(int x, int y, int button){
-
+    
 }
 
 //--------------------------------------------------------------
 void ofApp::mouseEntered(int x, int y){
-
+    
 }
 
 //--------------------------------------------------------------
 void ofApp::mouseExited(int x, int y){
-
+    
 }
 
 //--------------------------------------------------------------
 void ofApp::windowResized(int w, int h){
-
+    
 }
 
 //--------------------------------------------------------------
 void ofApp::gotMessage(ofMessage msg){
-
+    
 }
 
 //--------------------------------------------------------------
-void ofApp::dragEvent(ofDragInfo dragInfo){ 
-
+void ofApp::dragEvent(ofDragInfo dragInfo){
+    
 }
 
 void ofApp::nameStuff(){
